7eb03264a44d6f58133dfe4ceac983368d3e320c
Mohammed Azfar <m_az@live.in>
2021-08-21 16:21:42 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ docker build -t test .
 
 docker tag test ghcr.io/notopss/copenhagen:latest
 
+echo $MY_PAT
+
 echo $MY_PAT | docker login ghcr.io -u macbash --password-stdin
 
 docker push ghcr.io/notopss/copenhagen:latest
